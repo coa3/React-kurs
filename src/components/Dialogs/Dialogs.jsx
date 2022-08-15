06f04442d319029c5de39256da055416f62f29f0
coa3 <coa3@mail.ru>
2022-08-15 22:47:14 +0300

    leson22

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import c from './Dialogs.module.css'
 
 const Dialogs = (props) => {
   return (
     <div className={c.dialogs}>
       <div className={c.dialogsItems}>
-        <div className={`${c.dialog} ${c.active}`}>Demian</div>
-        <div className={c.dialog}>Oleg</div>
-        <div className={c.dialog}>Anastasia</div>
-        <div className={c.dialog}>Natalyia</div>
+        <div className={`${c.dialog} ${c.active}`}>
+          <NavLink to="/dialogs/1">Demian</NavLink>
+        </div>
+        <div className={c.dialog}>
+          <NavLink to="/dialogs/2"> Oleg</NavLink>
+        </div>
+        <div className={c.dialog}>
+          <NavLink to="/dialogs/3">Anastasia</NavLink>
+        </div>
+        <div className={c.dialog}>
+          <NavLink to="/dialogs/4">Natalyia</NavLink>
+        </div>
       </div>
       <div className={c.messages}>
         <div className={c.dialog}>HI</div>
